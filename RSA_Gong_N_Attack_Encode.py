ffa7cbd55a813cd66d81738ac89215726645ac77
maybeISaboy <82251431+21r000@users.noreply.github.com>
2022-09-26 18:22:20 +0800

    Update RSA_Gong_N_Attack_Encode.py

--- RSA_Gong_N_Attack_Encode.py
+++ RSA_Gong_N_Attack_Encode.py
@@ -1,11 +1,11 @@
 from Crypto.Util.number import *
 
-flag = b'flag{9v1pqsdkhg-qcsfzlt0-giprj3xhc7}'
+flag = b''
 
 p = getPrime(1024)
 q = getPrime(1024)
-e1 = 2333
-e2 = 23333
+e1 = 
+e2 = 
 m = bytes_to_long(flag)
 n = p * q
 c1 = pow(m, e1, n)
